66e1ae767799c77d82ce6e952388f0001f6ed523
JiWoo Kim <worldkes@ajou.ac.kr>
2017-02-03 02:44:41 +0900

    기능추가   * 시작페이지 버튼 추가

--- app/src/main/java/tk/twpooi/happycake/StartActivity.java
+++ app/src/main/java/tk/twpooi/happycake/StartActivity.java
@@ -3,11 +3,26 @@ package tk.twpooi.happycake;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+
 public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Button parisBaguette = (Button)findViewById(R.id.parisBaguette);
+        parisBaguette.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        MyTwo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
